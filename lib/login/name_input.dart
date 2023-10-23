9d43f2b6061a505e35d15db50b513cc3681ec4be
Mihkel Tiganik <mtiganik@gmail.com>
2023-10-23 12:49:20 +0300

    name input fields

---
+++ lib/login/name_input.dart
@@ -0,0 +1,55 @@
+
+
+import 'package:flutter/material.dart';
+
+class NameInput extends StatefulWidget{
+
+  const NameInput({super.key, required this.label, required this.onInputChanged});
+  final String label;
+  final Function(String) onInputChanged;
+  
+  @override
+  State<StatefulWidget> createState() => _NameInputState();
+
+}
+
+class _NameInputState extends State<NameInput>{
+final TextEditingController nameController = TextEditingController();
+
+@override
+void initState(){
+  super.initState();
+  nameController.addListener(() {
+    widget.onInputChanged(nameController.text);
+  });
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          widget.label,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        TextField(
+          controller: nameController,
+          onChanged: widget.onInputChanged,
+          decoration: InputDecoration(
+            hintText: 'Enter ${widget.label}',
+          )
+        )
+      ],
+    );
+  }
+  @override
+  void dispose(){
+    nameController.dispose();
+    super.dispose();
+  }
+  
+}
--- lib/login/register_page.dart
+++ lib/login/register_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/login/email_text_input.dart';
+import 'package:flutter_todo_app/login/login_header.dart';
 import 'package:flutter_todo_app/login/login_page.dart';
+import 'package:flutter_todo_app/login/name_input.dart';
 import 'package:flutter_todo_app/login/page_navigation.dart';
 
 class RegisterPage extends StatelessWidget{
@@ -45,6 +48,29 @@ class RegisterPageBox extends StatefulWidget{
 }
 
 class _RegisterPageBoxState extends State<RegisterPageBox>{
+  String firstname = '';
+  String lastname = '';
+  String email = '';
+  String password = '';
+  String confirmPassword = '';
+
+  void handleFirstnameChanged(String newFirstname){
+    setState((){
+      firstname = newFirstname;
+    });
+  }
+
+  void handleLastnameChanged(String newLastname){
+    setState((){
+      lastname = newLastname;
+    });
+  }
+
+  void handleEmailChanged(String newEmail){
+    setState((){
+      email = newEmail;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -55,10 +81,13 @@ class _RegisterPageBoxState extends State<RegisterPageBox>{
       padding: const EdgeInsets.all(16.0),
       margin: const EdgeInsets.all(35.0),
 
-      child: const Column(
+      child: Column(
       children:  <Widget>[
-       Text("In register page"),
-      PageNavigation(title: "Have account? Login instead", targetPage: LoginPage(),)
+      const LoginHeader(title: "Register"),
+      NameInput(label: "First Name", onInputChanged: handleFirstnameChanged),
+      NameInput(label: "Last Name", onInputChanged: handleLastnameChanged),
+      EmailTextInput(labelText: "Email", onEmailChanged: handleEmailChanged),
+      const PageNavigation(title: "Have account? Login instead", targetPage: LoginPage(),)
  
       ]
       ));
