0c274be0ed2645b4e0fbdb50ac678a96f435a3a6
Mihkel Tiganik <mtiganik@gmail.com>
2023-11-07 22:41:55 +0200

    moved cat list from one folder ahead

--- lib/home/categories/categories_home.dart
+++ lib/home/categories/categories_home.dart
@@ -1,32 +1,68 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/api/api_methods/category_api.dart';
 import 'package:flutter_todo_app/home/categories/add_category_page.dart';
 import 'package:flutter_todo_app/home/categories/categories_list.dart';
+import 'package:flutter_todo_app/home/categories/category_list_item.dart';
+import 'package:flutter_todo_app/models/category.dart';
 
-class CategoriesHome extends StatelessWidget{
+class CategoriesHome extends StatefulWidget {
   const CategoriesHome({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _CategoriesHomeState();
+}
+
+class _CategoriesHomeState extends State<CategoriesHome> {
+  Future<List<Category>?>? categoriesFuture;
+  @override
+  void initState() {
+    super.initState();
+    categoriesFuture = CategoryApi.getAllCategories();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Categories", style: TextStyle(color:Colors.white)),
-        backgroundColor: Colors.blue,
-      ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:[
-              ElevatedButton(onPressed: (){Navigator.push(context, MaterialPageRoute(builder: ((context) =>  AddCategoryPage()))); }, child: const Text("Add new")),
-            ]
-          ),
-          const CategoriesList(),
-        ],
-      )
-    );
-
+        appBar: AppBar(
+          title:
+              const Text("Categories", style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blue,
+        ),
+        body: Column(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: ((context) => AddCategoryPage())));
+                  },
+                  child: const Text("Add new")),
+            ]),
+            FutureBuilder(
+                future: categoriesFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text('Error: ${snapshot.error}'),
+                    );
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(child: Text("No categories available"));
+                  } else {
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        Category category = snapshot.data![index];
+                        return CategoryListItem(category: category);
+                      },
+                    );
+                  }
+                }),
+          ],
+        ));
   }
-
 }
