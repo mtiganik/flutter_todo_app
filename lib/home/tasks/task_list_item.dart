2933dabab7fe087bebdc5806834e872884e44cdf
Mihkel Tiganik <mtiganik@gmail.com>
2023-11-13 10:35:20 +0200

    workings with tasklistItem

--- lib/home/tasks/task_list_item.dart
+++ lib/home/tasks/task_list_item.dart
@@ -6,6 +6,8 @@ import 'package:flutter_todo_app/models/category.dart';
 import 'package:flutter_todo_app/models/priority.dart';
 import 'package:flutter_todo_app/models/task.dart';
 
+enum MenuItems {markAsDone, edit, archieve, delete}
+
 class TaskListItem extends StatefulWidget{
   final Task task;
   final Category taskCategory;
@@ -18,38 +20,65 @@ class TaskListItem extends StatefulWidget{
 }
 
 class _TaskListItemState extends State<TaskListItem>{
+  MenuItems? selectedMenuItem;
 
+  Future<void> handleMarkAsDone()async{
+    print("On handle mark as done");
+  }
   TextStyle getPriorityStyle(Priority priority){
     if(priority.prioritySort >= 10) {return const TextStyle(color: Colors.orange, fontWeight: FontWeight.bold);} 
     else if(priority.prioritySort >= 5){return const TextStyle();}
     else {return const TextStyle(color: Colors.lightGreen);}
   } 
+
+  Icon getIconByCategory(Category category){
+    if(category.categoryName == "Home") {return const Icon(Icons.home);}
+    else if(category.categoryName == "Work") {return const Icon(Icons.work);}
+    else if(category.categoryName == "School") {return const Icon(Icons.school);}
+    else if(category.categoryName == "Other") {return const Icon(Icons.other_houses);}
+    else {return const Icon(Icons.question_mark);}
+  }
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(widget.task.taskName),
+      leading: getIconByCategory(widget.taskCategory),
       subtitle: Text.rich(
         TextSpan(
           children: <TextSpan>[
-            TextSpan(text: "Created: ${previousTimeToString(widget.task.createdDt)}, "),
+            TextSpan(text: "${previousTimeToString(widget.task.createdDt)}, "),
             TextSpan(text: "${widget.taskPriority.priorityName}\n", style: getPriorityStyle(widget.taskPriority)),
             TextSpan(text: "Due: ${futureTimeToString(widget.task.dueDt)}"),
-            const TextSpan(text: "\nDue:"),
           ]
         )
       ),
+      trailing: PopupMenuButton<MenuItems>(
+        onSelected: (MenuItems result){
+          if(result == MenuItems.markAsDone){
+            handleMarkAsDone();
+          }
+
+        },
+        itemBuilder: (BuildContext context) => <PopupMenuEntry<MenuItems>>[
+          const PopupMenuItem<MenuItems>(
+            value: MenuItems.markAsDone,
+            child: Text("Mark as done")),
+          
+          const PopupMenuItem<MenuItems>(
+            value: MenuItems.edit,
+            child: Text("Edit")),
+          
+          const PopupMenuItem<MenuItems>(
+            value: MenuItems.archieve,
+            child: Text("Archive")),
+          
+          const PopupMenuItem<MenuItems>(
+            value: MenuItems.delete,
+            child: Text("Delete"),
+          )
+        ],
+      )
     );
-
   }
-
 }
-
-//   TextSpan(
-//     text: 'Hello\n', // default text style
-//     children: <TextSpan>[
-//       TextSpan(text: ' beautiful ', style: TextStyle(fontStyle: FontStyle.italic)),
-//       TextSpan(text: 'world', style: TextStyle(fontWeight: FontWeight.bold)),
-//     ],
-//   ),
-// ),
 
